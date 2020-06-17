aded63bf2c0593b799cecfcba12ebb18fdf718dc
zhaoyongchao666 <1620397804@qq.com>
2020-06-17 14:05:18 +0800

    idea本地工程添加到git

---
+++ GitSend/src/com/rj/bd/Gits.java
@@ -0,0 +1,12 @@
+package com.rj.bd;
+
+public class Gits {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
